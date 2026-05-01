991452eec4314029d4869a9c6e9a9311c49642f3
Oscar Oguledo <OSCARCHIAGOZIEM@GMAIL.COM>
2026-05-01 10:33:11 -0400

    Move backend URL configuration to config file
    - Created core/config.js with port and host settings
    - Updated server.js to use config instead of hardcoded values
    - Supports environment variables for PORT and HOST

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 // server.js
 const express = require('express');
+const config = require('./core/config');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(8000, () => {
-  console.log('Server running at http://localhost:8000');
+app.listen(config.port, () => {
+  console.log(`Server running at http://${config.host}:${config.port}`);
 });
